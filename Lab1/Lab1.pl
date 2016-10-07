7286616d435e5f7486b065caf895e46bd0f356cf
Thony Price <Thony@n164-p153.kthopen.kth.se>
2016-10-07 14:26:58 +0200

    La till regelappliceringar från Lab

--- Lab1/Lab1.pl
+++ Lab1/Lab1.pl
@@ -1,2 +1,18 @@
 % William Skagerström, wska@kth.se
 % Thony Price, asdasdasd
+
+/*
+Regelappliceringar:
+
+premise premise               impel(x,y) →e x,y
+assumption assumption         negint(x,y) ¬i x-y
+copy(x) copy x                negel(x,y) ¬e x,y
+andint(x,y) ∧i x, y           contel(x) ⊥e x
+andel1(x) ∧e1 x               negnegint(x) ¬¬i x
+andel2(x) ∧e2 x               negnegel(x) ¬¬e x
+orint1(x) ∨i1 x               mt(x,y) mt x,y
+orint2(x) ∨i2 x               pbc(x,y) pbc x-y
+orel(x,y,u,v,w) ∨e x,y–u,v–w  lem lem
+impint(x,y) →i x–y
+
+*/
