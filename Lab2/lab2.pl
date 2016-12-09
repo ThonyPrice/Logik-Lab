7f9560dd0cdd8904000f0089b918575210b7d8e5
Thony Price <thonyprice@gmail.com>
2016-12-09 10:31:23 +0100

    - Unnecessary comments

--- Lab2/lab2.pl
+++ Lab2/lab2.pl
@@ -1,22 +1,30 @@
+% ~~~ * Info  * ~~~%
+
 /*
+Project:            Lab2 - Logic in computer science
+Authors:            Thony Price
+                    Wlliam Skagerström
+Last revision:      2016-12-09
+*/
+
+% ~~~ * Main predicate * ~~~ %
+
 % Programskelett
-% For SICStus, uncomment line below: (needed for member/2)
-%:- use_module(library(lists)).
 % Load model, initial state and formula from file.
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
     check(T, L, S, [], F).
-% check(T, L, S, U, F)
-%     T - The transitions in form of adjacency lists
-%     L - The labeling
-%     S - Current state
-%     U - Currently recorded states
-% F - CTL Formula to check.
-%
+%   check(T, L, S, U, F)
+%       T - The transitions in form of adjacency lists
+%       L - The labeling
+%       S - Current state
+%       U - Currently recorded states
+%       F - CTL Formula to check.
+
 % Should evaluate to true iff the sequent below is valid. %
-% (T,L), S |- F
-%U
-% To execute: consult(’your_file.pl’). verify(’input.txt’).
+% (T,L), S |-   F
+%             U
+
 % Literals
 %check(_, L, S, [], X)      :- ...
 %check(_, L, S, [], neg(X)) :- ...
@@ -29,4 +37,3 @@ verify(Input) :-
 % EG
 % EF
 % AF
-*/
